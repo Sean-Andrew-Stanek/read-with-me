f65e64d5cbed240007f5cdb841e5f1d83d022638
Sogol <sogol.saadat@gmail.com>
2025-03-09 08:09:49 -0600

    removed client id and secret for development testing

--- src/auth.ts
+++ src/auth.ts
@@ -3,10 +3,11 @@ import Google from 'next-auth/providers/google';
 
 // Initialize NextAuth with Google as an authentication provider
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Google({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-        })
-    ]
+    // providers: [
+    //     Google({
+    //         clientId: process.env.GOOGLE_CLIENT_ID!,
+    //         clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    //     })
+    // ]
+    providers: [Google]
 });
