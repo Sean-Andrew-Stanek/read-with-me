62030c6644c7ef3664563fb72e42622f65b533c4
Sogol <sogol.saadat@gmail.com>
2025-03-10 10:27:24 -0600

    Added responsiveness to buttons and texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import { Button } from '@/components/ui/button';
 const Home: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
-            <h1 className="text-3xl font-bold">Welcome to Read With Me</h1>
-            <p className="text-gray-600 mt-2">Temporary landing page.</p>
+            <h1 className="text-2xl  md:text-4xl  font-bold">
+                Welcome to Read With Me
+            </h1>
+            <p className="text-gray-600 mt-2 text-lg md:text-xl lg:text-2xl">
+                Temporary landing page.
+            </p>
             <Link href="/signin">
-                <Button className="mt-4 px-4 py-2 bg-red-400 text-white rounded">
+                <Button className="mt-4 md:w-auto py-3 md:py-6 lg:py-6 md:text-xl text-base bg-red-400 text-white rounded font-medium">
                     Get Started
                 </Button>
             </Link>
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -12,7 +12,7 @@ const SignIn = async (): Promise<JSX.Element> => {
         <div className="flex flex-col items-center justify-center min-h-screen text-center">
             {user ? (
                 <>
-                    <h1 className="text-3xl font-semibold">
+                    <h1 className="text-2xl  md:text-4xl font-semibold">
                         Welcome {user.name}
                     </h1>
                     <form
@@ -22,7 +22,7 @@ const SignIn = async (): Promise<JSX.Element> => {
                         }}
                     >
                         <Button
-                            className="bg-red-400 text-white px-4 py-2 rounded mt-4"
+                            className="mt-4 md:w-auto py-3 md:py-6 lg:py-6 md:text-xl text-base bg-red-400 text-white rounded font-medium"
                             type="submit"
                         >
                             Sign Out
@@ -31,7 +31,7 @@ const SignIn = async (): Promise<JSX.Element> => {
                 </>
             ) : (
                 <>
-                    <h1 className="text-3xl font-semibold">
+                    <h1 className="text-2xl  md:text-4xl p-4 font-semibold">
                         You are not authenticated. Please sign in!
                     </h1>
                     <form
@@ -40,7 +40,10 @@ const SignIn = async (): Promise<JSX.Element> => {
                             await signIn('google');
                         }}
                     >
-                        <Button className="bg-[#131314] border mt-4 border-[#8E918F] text-[#E3E3E3] font-medium text-[14px] leading-[20px] rounded-full px-6 py-4 flex items-center gap-2 shadow-md hover:bg-[#1a1a1b] transition">
+                        <Button
+                            className="bg-[#131314] border mt-4 border-[#8E918F] text-[#E3E3E3] font-medium text-[14px] md:text-[16px] lg:text-[18px] 
+ leading-[20px] md:leading-[24px] lg:leading-[26px] rounded-full px-6 py-4 md:px-12 md:py-5 lg:px-14 lg:py-6 flex items-center gap-2 shadow-md hover:bg-[#1a1a1b] transition"
+                        >
                             <FcGoogle size={18} />
                             Sign In with Google
                         </Button>
