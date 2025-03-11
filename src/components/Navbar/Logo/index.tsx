8013f9466ebd15c5eefd4576fd2e8f30fa06db1a
Sean Andrew Stanek <sean.stanek@nebulas-software.com>
2025-03-11 12:13:15 +0900

    Navbar rework
    Components move to their own file
    Structure reworked

---
+++ src/components/Navbar/Logo/index.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Logo: React.FC = () => {
+    return (
+        <Link href="/" className="text-2xl font-bold">
+            Read With Me
+        </Link>
+    );
+};
+
+export default Logo;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,11 +1,9 @@
-import Link from 'next/link';
+import Logo from './Logo';
 
 const Navbar: React.FC = () => {
     return (
         <nav className="flex justify-between py-4 bg-red-400 text-white p-4">
-            <Link href="/" className="text-2xl font-bold">
-                Read With Me
-            </Link>
+            <Logo />
         </nav>
     );
 };
