01ee1415446a178426a3107c1a8d68aef4f2a4fd
Alexis Abner <alexiproj@gmail.com>
2025-03-24 15:15:10 -0600

    Create dashboard folder for testing

--- tests/homepage/home.spec.ts
+++ tests/homepage/home.spec.ts
@@ -1,5 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+test('should have welcome text', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.getByText('Welcome to Read With Me')).toBeVisible();
+});
+
 test('should have correct metadata', async ({ page }) => {
   await page.goto('/');
 
@@ -20,12 +26,6 @@ test('should lead to google signin button', async ({ page }) => {
   await signinBtn.click();
 
   await expect(page.getByRole('button', {name: /Continue with Google/i})).toBeVisible();
-});
-
-test('should have welcome text', async ({ page }) => {
-  await page.goto('/');
-
-  await expect(page.getByText('Welcome to Read With Me')).toBeVisible();
 });
 
 
