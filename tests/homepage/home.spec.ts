e627c5576c1f97821cb5a6e2e74c65573c435e58
Alexis Abner <alexiproj@gmail.com>
2025-03-24 14:42:32 -0600

    Test signin button click

--- tests/homepage/home.spec.ts
+++ tests/homepage/home.spec.ts
@@ -13,6 +13,15 @@ test('should contain the signin button', async ({ page }) => {
   await expect(page.getByRole('button', {name: /Sign In/i})).toBeVisible();
 });
 
+test('should lead to google signin button', async ({ page }) => {
+  await page.goto('/');
+  const signinBtn = page.getByRole('button', {name: /Sign In/i});
+
+  await signinBtn.click();
+
+  await expect(page.getByRole('button', {name: /Continue with Google/i})).toBeVisible();
+});
+
 test('should have welcome text', async ({ page }) => {
   await page.goto('/');
 
