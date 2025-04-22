2b42bf9d72a5f40a0f75dedb74062aa940c717dc
Alexis Abner <alexiproj@gmail.com>
2025-04-21 22:54:55 -0600

    Add button 'Back' to '/create-story' path to direct user '/home'

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,9 +8,13 @@ import { signOut } from 'next-auth/react';
 import { LogOut } from 'lucide-react';
 import { useOnboardingStore } from '@/lib/store/onboardingStore';
 import OnboardingDialog from '@/components/OnBoardingDialog';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
     const { showOnboarding, setShowOnboarding } = useOnboardingStore();
+    const pathname = usePathname();
+    const isCreateStoryPage = pathname === '/create-story';
 
     const { data: session, status } = useSession();
     if (status === 'loading') return null;
@@ -24,10 +28,17 @@ const Navbar: React.FC = () => {
 
                 <div className="flex items-center gap-2 ">
                     {isLoggedIn ? (
+                        
                         <div className="flex items-center gap-2">
                             <span className="text-white text-sm md:text-base break-words whitespace-normal">
                                 {session?.user?.name}
                             </span>
+
+                            {isCreateStoryPage && (
+                                <Link href='/home'>
+                                    <Button variant="outline" className='text-black cursor-pointer'>Back</Button>
+                                </Link>
+                            )}
 
                             <Button
                                 onClick={() => signOut({ callbackUrl: '/' })}
