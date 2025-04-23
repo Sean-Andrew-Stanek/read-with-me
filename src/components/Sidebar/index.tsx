1b79db83c8081e18471a2c3e5204814d379343cb
Alexis Abner <alexiproj@gmail.com>
2025-04-22 18:25:32 -0600

    Rename 'Settings' btn on Sidebar to 'Profile'

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -8,9 +8,9 @@ const Sidebar: FC = () => {
         <aside className="w-64 h-screen bg-white border-r border-gray-200 top-16 left-0 z-40 p-4 shadow-sm font-literata-variable font-semibold text-xl">
             {/* ^ top-16 to be placed under a navbar that is 64px tall */}
             <nav className="space-y-4">
+                <DashboardSidebarLink href="#" title="Profile" />
                 <DashboardSidebarLink href="/create-story" title="Create Story" />
                 <DashboardSidebarLink href="/my-stories" title="My Stories" />
-                <DashboardSidebarLink href="#" title="Settings" />
             </nav>
         </aside>
     );
