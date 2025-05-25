6a244460680a87fe36043e212bd396508480681e
Sogol <sogol.saadat@gmail.com>
2025-05-24 19:00:53 -0500

    temporary first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { auth } from '@/auth';
 import { redirect } from 'next/navigation';
 import { frontPageStrings as strings } from '@/config/strings';
 import { navLink } from '@/config/navigation';
+import Image from 'next/image';
 
 const Home: React.FC = async () => {
     const session = await auth();
@@ -11,12 +12,19 @@ const Home: React.FC = async () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-900">
-            <h1 className="text-lg md:text-4xl  font-bold">
+            <h1 className="text-lg md:text-5xl font-bold mt-10">
                 {strings.welcome}
             </h1>
-            <p className="text-gray-600 mt-2 text-lg md:text-xl lg:text-2xl">
+            {/* <p className="text-gray-600 mt-2 text-lg md:text-xl lg:text-2xl">
                 {strings.description}
-            </p>
+            </p> */}
+            <Image
+                src="/simple-logo.png"
+                alt="Read With Me First Page"
+                width={600}
+                height={400}
+                className="mt-3 rounded-lg"
+            />
         </div>
     );
 };
--- src/config/strings.ts
+++ src/config/strings.ts
@@ -1,5 +1,5 @@
 export const frontPageStrings: Record<string, string> = {
     description: 'Temporary landing page.',
-    welcome: 'Welcome to Read With Me',
+    welcome: 'Read With Me',
     getStarted: 'Get Started'
 };
