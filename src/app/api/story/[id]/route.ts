3d6cc91cce1f7bf01aef613bf8a0d66a03251a52
Sean Andrew Stanek <sean.andrew.stanek@gmail.com>
2025-06-02 22:08:18 +0900

    fix: use NextRequest

--- src/app/api/story/[id]/route.ts
+++ src/app/api/story/[id]/route.ts
@@ -1,37 +1,33 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/auth';
 import clientPromise from '@/lib/mongodb';
 
 export const GET = async (
-    _request: Request,
-    context: { params: { id: string } }
+    _request: NextRequest,
+    { params }: { params: { id: string } }
 ): Promise<Response> => {
     try {
         const session = await auth();
         if (!session || !session.user?.uuid) {
             return NextResponse.json(
-                {
-                    error: 'Unauthorized'
-                },
+                { error: 'Unauthorized' },
                 { status: 401 }
             );
         }
 
-        const { id } = context.params;
+        const { id } = params;
         const client = await clientPromise;
         const db = client.db('read-with-me');
 
-        //Searching story using id
         const story = await db.collection('stories').findOne({ id });
 
         if (!story) {
             return NextResponse.json(
-                {
-                    error: 'Story not found in db.'
-                },
+                { error: 'Story not found in db.' },
                 { status: 404 }
             );
         }
+
         return NextResponse.json(story, { status: 200 });
     } catch (error) {
         return NextResponse.json(
