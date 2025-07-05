23b6eb88e6fe13f6b016ae01991bcb2251ab7db0
Alexis Abner <alexiproj@gmail.com>
2025-07-04 23:32:57 -0600

    Add buttons to parent dashboard

--- src/app/parent-dashboard/page.tsx
+++ src/app/parent-dashboard/page.tsx
@@ -1,9 +1,43 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { JSX } from 'react'
 
 const ParentDashboard = (): JSX.Element => {
-  return (
-    <div className='flex flex-col items-center text-4xl'>Parent Dashboard</div>
-  )
+    return (
+        <>
+            <div className="text-2xl">Parent Dashboard</div>
+            <div className="flex flex-col space-y-8 mr-0 p-0 text-2xl w-[30%] bg-gray-200">
+                <Link href="/story-board">
+                    <Button
+                        className="w-[45%] lg:w-[90%] pl-0 h-16 group justify-start ml-9 text-2xl text-gray-700 hover:text-white bg-transparent hover:bg-yellow-400 shadow-none rounded-4xl cursor-pointer"
+                    >
+                        View a child
+                    </Button>
+                </Link>
+                <Link href="/story-board">
+                    <Button
+                        className="w-[45%] lg:w-[90%] pl-0 h-16 group justify-start ml-9 text-2xl text-gray-700 hover:text-white bg-transparent hover:bg-yellow-400 shadow-none rounded-4xl cursor-pointer"
+                    >
+                        Add Restrictions
+                    </Button>
+                </Link>
+                <Link href="/story-board">
+                </Link>
+                <Link href="/story-board">
+                <Button className="w-[45%] lg:w-[90%] pl-0 h-16 group justify-start ml-9 text-2xl text-gray-700 hover:text-white bg-transparent hover:bg-yellow-400 shadow-none rounded-4xl cursor-pointer"
+                >
+                    Add a child
+                </Button>
+                </Link>
+                <Link href="/story-board">
+                    <Button className="w-[45%] lg:w-[90%] pl-0 h-16 group justify-start ml-9 text-2xl text-gray-700 hover:text-white bg-transparent hover:bg-yellow-400 shadow-none rounded-4xl cursor-pointer"
+                    >
+                        Remove a child
+                    </Button>
+                </Link>
+            </div>
+        </>
+    )
 }
 
 export default ParentDashboard;
