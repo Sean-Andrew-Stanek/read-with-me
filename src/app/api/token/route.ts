b2c6d411c281e906594075d5eefc0337cc2352c1
Sogol <sogol.saadat@gmail.com>
2025-07-05 14:27:27 -0500

    added PUT api for child to submit token and link to parent

--- src/app/api/token/route.ts
+++ src/app/api/token/route.ts
@@ -21,11 +21,58 @@ export const POST = async (_req: NextRequest): Promise<NextResponse> => {
     };
 
     try {
+        // validating  tokenData before inserting
         const validated = LinkTokenSchema.parse(tokenData);
         const client = await clientPromise;
         const db = client.db('read-with-me');
         await db.collection('linkt_tokens').insertOne(validated);
         return NextResponse.json({ token: validated.token }, { status: 201 });
+    } catch {
+        return NextResponse.json(
+            { error: 'Invalid token data' },
+            { status: 400 }
+        );
+    }
+};
+
+// Child submit the token to link to the parent
+export const PUT = async (req: NextRequest): Promise<NextResponse> => {
+    const session = await auth();
+
+    if (!session?.user?.uuid || session.user.isParent) {
+        return NextResponse.json({ erro: 'Unathorized' }, { status: 401 });
+    }
+
+    const { token } = await req.json();
+    const client = await clientPromise;
+    const db = client.db('read-with-me');
+
+    const rawToken = await db.collection('link-tokens').findOne({ token });
+    if (!rawToken) {
+        return NextResponse.json({ error: 'Token not found' }, { status: 404 });
+    }
+    try {
+        // validating raw token
+        const linkToken = LinkTokenSchema.parse(rawToken);
+        if (linkToken.isUsed || linkToken.expiresAt < new Date()) {
+            return NextResponse.json(
+                { error: 'Token is invalid or expired' },
+                { status: 400 }
+            );
+        }
+
+        // find the child's uuid and set their parent to the one with token
+        await db
+            .collection('childUsers')
+            .updateOne(
+                { uuid: session.user.uuid },
+                { $set: { parentId: linkToken.parentId } }
+            );
+
+        await db
+            .collection('link_tokens')
+            .updateOne({ token }, { $set: { isUsed: true } });
+        return NextResponse.json({ message: 'Linked successfully' });
     } catch {
         return NextResponse.json(
             { error: 'Invalid token data' },
