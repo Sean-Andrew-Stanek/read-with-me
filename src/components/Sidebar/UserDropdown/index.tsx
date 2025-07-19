d3ce3e36a366540bea37d8c1dc15d515869c97bf
Alexis Abner <alexiproj@gmail.com>
2025-07-18 18:10:34 -0600

    Update dropdown menu 'elipsis' for navigation

--- src/components/Sidebar/UserDropdown/index.tsx
+++ src/components/Sidebar/UserDropdown/index.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/navigation';
-import { Ellipsis, LogOut, User } from 'lucide-react';
+import { usePathname, useRouter } from 'next/navigation';
+import { Ellipsis, Home, LogOut, User } from 'lucide-react';
 import { signOut } from 'next-auth/react';
 import {
     DropdownMenu,
@@ -10,34 +10,48 @@ import {
 import { FC, JSX } from 'react';
 
 const UserDropdown: FC = (): JSX.Element => {
-    const router = useRouter();
+    const pathname = usePathname();
+  const router = useRouter();
+
+  const isOnProfile = pathname === '/profile';
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <div className="w-5 h-5 flex items-center justify-center rounded-full bg-white text-xl font-bold text-gray-700 leading-none select-none cursor-pointer hover:text-gray-900">
-                    <Ellipsis />
-                </div>
-            </DropdownMenuTrigger>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div className="w-5 h-5 flex items-center justify-center rounded-full bg-white text-xl font-bold text-gray-700 leading-none select-none cursor-pointer hover:text-gray-900">
+          <Ellipsis />
+        </div>
+      </DropdownMenuTrigger>
 
-            <DropdownMenuContent align="end" className="w-40 ">
-                <DropdownMenuItem
-                    onClick={() => router.push('/profile')}
-                    className="text-black cursor-pointer"
-                >
-                    <User className="mr-2 h-4 w-4" />
-                    Profile
-                </DropdownMenuItem>
+      <DropdownMenuContent align="end" className="w-40">
+        {!isOnProfile ? (
+          <DropdownMenuItem
+            onClick={() => router.push('/profile')}
+            className="text-black cursor-pointer"
+          >
+            <User className="mr-2 h-4 w-4" />
+            Profile
+          </DropdownMenuItem>
+        ) : (
+          <DropdownMenuItem
+            onClick={() => router.push('/')}
+            className="text-black cursor-pointer"
+          >
+            <Home className="mr-2 h-4 w-4" />
+            Home
+          </DropdownMenuItem>
+        )}
 
-                <DropdownMenuItem
-                    onClick={() => signOut({ callbackUrl: '/' })}
-                    className="text-black cursor-pointer"
-                >
-                    <LogOut className="mr-2 h-4 w-4" /> Sign Out
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
-    );
+        <DropdownMenuItem
+          onClick={() => signOut({ callbackUrl: '/' })}
+          className="text-black cursor-pointer"
+        >
+          <LogOut className="mr-2 h-4 w-4" />
+          Sign Out
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 };
 
 export default UserDropdown;
