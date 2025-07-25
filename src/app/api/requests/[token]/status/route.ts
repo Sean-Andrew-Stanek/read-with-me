1e29b4c5596bc3c23e4b6ab2e66255c53e141f6c
Sogol <sogol.saadat@gmail.com>
2025-07-24 20:57:15 -0500

    Added API to Update the request status

---
+++ src/app/api/requests/[token]/status/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server';
+import clientPromise from '@/lib/mongodb';
+import { auth } from '@/auth';
+
+export const PATCH = async (
+    req: Request,
+    { params }: { params: { token: string } }
+) => {
+    const session = await auth();
+    if (!session?.user?.uuid || !session?.user?.isParent) {
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+
+    const { token } = params;
+    const body = await req.json();
+    const { status } = body;
+
+    if (!['rejected', 'approved'].includes(status)) {
+        return NextResponse.json({ error: 'Invalid status' }, { status: 400 });
+    }
+
+    const client = await clientPromise;
+    const db = client.db('read-with-me');
+
+    const result = await db
+        .collection('link_requests')
+        .findOneAndUpdate(
+            { token, parentId: session?.user?.uuid },
+            { $set: { status } },
+            { returnDocument: 'after' }
+        );
+
+    if (!result || !result.value) {
+        return NextResponse.json(
+            { error: 'Request not found' },
+            { status: 404 }
+        );
+    }
+
+    return NextResponse.json({
+        message: 'Status updated',
+        request: result.value
+    });
+};
