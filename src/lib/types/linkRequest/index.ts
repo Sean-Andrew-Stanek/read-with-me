4cc0b67f1249d3871297549e82f929cc5150af48
Sogol <sogol.saadat@gmail.com>
2025-07-26 14:41:33 -0500

    Added API for pending requests, to not show the enter token link in child profile once the child enters the token and waiting for approval

--- src/lib/types/linkRequest/index.ts
+++ src/lib/types/linkRequest/index.ts
@@ -9,11 +9,6 @@ export const LinkRequestSchema = z.object({
     status: z.enum(['pending', 'approved', 'rejected'])
 });
 
-export const LinkStatusSchema = z.object({
-    isWaitingApproval: z.boolean()
-});
-
-export type LinkStatus = z.infer<typeof LinkStatusSchema>;
 export const LinkRequestArraySchema = z.array(LinkRequestSchema);
 export type LinkRequest = z.infer<typeof LinkRequestSchema>;
 export type LinkRequestArray = z.infer<typeof LinkRequestArraySchema>;
