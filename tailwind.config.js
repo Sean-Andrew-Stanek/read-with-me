c476920a5575c567c9cba7bf5b586f30b711ab95
Alexis Abner <alexiproj@gmail.com>
2025-08-02 21:35:47 -0600

    Fix ESLint issue in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
-import('tailwindcss').Config;
 import defaultTheme from 'tailwindcss/defaultTheme';
 
-
-export const content = [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-]
-export const theme = {
-    extend: {
-        fontFamily: {
-            sans: ['var(--font-roboto)', ...defaultTheme.fontFamily.sans], //Default font face
-            literata: ['var(--font-literata)', ...defaultTheme.fontFamily.serif], //Custom font face for reading material
+/** @type {import('tailwindcss').Config} */
+const config = {
+    content: [
+        './src/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['var(--font-roboto)', ...defaultTheme.fontFamily.sans], //Default font face
+                literata: ['var(--font-literata)', ...defaultTheme.fontFamily.serif], //Custom font face for reading material
+            },
         },
     },
-}
-export const plugins = []
+    plugins: []
+};
+
+export default config;
